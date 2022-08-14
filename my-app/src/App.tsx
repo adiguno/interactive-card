5600fcc6f31e65f7f88615bef6cefaa00ba81b16
adiguno <25011351+adiguno@users.noreply.github.com>
2022-08-14 17:14:56 -0400

    add light/dark theme system perference

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { InteractiveCard } from "./components/InteractiveCard";
 import bgImage from "./images/bg-main-desktop.png";
 function App() {
+  const [theme, setTheme] = useState("dark");
+  useEffect(() => {
+    if (window.matchMedia("(prefers-color-scheme: light").matches) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.add("light");
+    }
+  }, [theme]);
+
   return (
     // relative bg-white overflow-hidden
     <div className="App content-center">
       SOMETHING
-      <div className=" bg-gray-500">
+      <div className=" light:bg-gray-500 dark:bg-gray-900">
         <InteractiveCard></InteractiveCard>
       </div>
     </div>
